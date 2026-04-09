874a6e0bdc2d8a929b97dafe336c10f0ae319eec
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2026-04-09 17:35:48 +0530

    studied polyfills

--- Javascript/Basics/polyfill.js
+++ Javascript/Basics/polyfill.js
@@ -1,9 +1,39 @@
 const arr = [1,2,3,4,5,6]
 
-arr.forEach((element, index) => {
-    console.log(`${index} : ${element}`);   
-});
+// Array.prototype.myForEach = function(userFn){
+//     let originalArray = this
+//     for(let i=0; i<originalArray.length; i++){
+//         userFn(originalArray[i], i)
+//     }
+// }
 
-Array.prototype.myForeach = function(userFn){
-    
+// arr.myForEach(function (element, index){
+//     console.log(`${index} : ${element}`);   
+// });
+
+// Array.prototype.myMap = function(userFn){
+//     const newArray = []
+//     for(let i=0;i<this.length; i++){
+//         newArray.push(userFn(this[i]))
+//     }
+//     return newArray
+// }
+// const newMapArray = arr.myMap(function(element){
+//     return element*10
+// })
+// console.log(newMapArray);
+
+Array.prototype.myFilter = function(userFn){
+    let myFilterArray = []
+    for(let i=0; i<this.length; i++){
+        if(userFn(this[i]))
+            myFilterArray.push(this[i])
+    }
+    return myFilterArray
 }
+
+const filterArray = arr.myFilter((element)=>{
+    if(element%3==0)
+        return element
+})
+console.log(filterArray);

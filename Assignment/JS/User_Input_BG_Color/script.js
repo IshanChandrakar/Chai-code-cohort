f9780e40d8f600f48179f93d08e49cd4b4bdd671
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2026-05-14 22:13:15 +0530

    created change BG Color on user input

---
+++ Assignment/JS/User_Input_BG_Color/script.js
@@ -0,0 +1,23 @@
+const submit = document.querySelector("#submit");
+const addedButtons = document.querySelector("#addedButtons");
+const body = document.querySelector("body")
+
+submit.addEventListener("click", () => {
+    let bgColorValue = document.querySelector("#bgColorValue").value;
+    if (bgColorValue) {
+        let newBtn = document.createElement("button");
+        newBtn.textContent = bgColorValue;
+        newBtn.id = bgColorValue.toLowerCase();
+        newBtn.className = "changeBgButton";
+        newBtn.style.backgroundColor = bgColorValue
+        
+        addedButtons.append(newBtn);
+        document.querySelector("#bgColorValue").value = "";
+    }
+});
+
+addedButtons.addEventListener("click", (event) => {
+    let bgValue = event.target.id;
+    alert(bgValue);
+    body.style.backgroundColor = bgValue
+});

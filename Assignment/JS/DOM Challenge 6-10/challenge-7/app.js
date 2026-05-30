534fb10a7e7c603eb41b178c7e27d19eed9bb867
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2026-05-30 22:16:21 +0530

    made accordion button

--- Assignment/JS/DOM Challenge 6-10/challenge-7/app.js
+++ Assignment/JS/DOM Challenge 6-10/challenge-7/app.js
@@ -1,3 +1,14 @@
 /**
  * Write your challenge solution here
  */
+const accordionButton = document.querySelectorAll(".accordion-button")
+accordionButton.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        if(button.parentElement.classList.contains("active")){
+            button.parentElement.classList.remove("active")
+        }
+        else{
+            button.parentElement.classList.add("active")
+        }
+    })
+})

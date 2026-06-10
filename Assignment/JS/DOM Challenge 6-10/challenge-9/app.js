72857daed0134445e6548f110f95243f61ad5259
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2026-06-10 22:28:59 +0530

    created sliding menu

--- Assignment/JS/DOM Challenge 6-10/challenge-9/app.js
+++ Assignment/JS/DOM Challenge 6-10/challenge-9/app.js
@@ -1,3 +1,26 @@
 /**
  * Write your challenge solution here
  */
+const panel = document.querySelector(".panel")
+const toggleBtn = document.querySelector(".toggle-btn")
+let isPanelActive = false
+toggleBtn.addEventListener("click",()=>{
+    isPanelActive = !isPanelActive
+    if(isPanelActive){
+        panel.classList.add("active")
+        const closeBtn = document.querySelector(".close-btn")
+        closeBtn.addEventListener("click",()=>{
+            panel.classList.remove("active")
+            isPanelActive = !isPanelActive
+        })
+        const menuItem = document.querySelectorAll(".menu-item")
+        menuItem.forEach(item=>{
+            item.addEventListener("click",()=>{
+                alert("Yo")
+            })
+        })
+    }
+    else{
+        panel.classList.remove("active")
+    }
+})

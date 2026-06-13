c12cfd31bd79b2a6fd6b4d24f759eddc71d4f715
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2026-06-13 22:59:03 +0530

    created calender for mood tracker

--- Assignment/JS/Mood_Tracker/script.js
+++ Assignment/JS/Mood_Tracker/script.js
@@ -8,12 +8,63 @@ moods.forEach(mood => {
 })
 
 const submitBtn = document.querySelector("#submitBtn")
-submitBtn.addEventListener("click",()=>{
+submitBtn.addEventListener("click", () => {
     let dateSelected = document.querySelector("#dateInput").value
-    if(currentMood && dateSelected){
+    if (currentMood && dateSelected) {
         console.log(dateSelected, currentMood)
-        localStorage.setItem(dateSelected,currentMood)
+        localStorage.setItem(dateSelected, currentMood)
 
         document.querySelector("#dateInput").value = null
+    }
+})
+
+let today = new Date()
+let day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate()
+let month = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1
+let year = today.getFullYear()
+let daysInMonth = new Date(year, month, 0).getDate()
+
+let calender = document.querySelector("#calender")
+
+for (let i = 1; i <= daysInMonth; i++) {
+    let newDate = document.createElement("div")
+    newDate.textContent = `${i}-${month}-${year}`
+    newDate.setAttribute("id", `${i}-${month}-${year}`)
+    calender.appendChild(newDate)
+}
+
+const previousMonth = document.querySelector("#previousMonth")
+previousMonth.addEventListener("click", () => {
+    calender.replaceChildren()
+    if(month>1)
+        month = month - 1
+    else if(month==1){
+        month = 12
+        year--
+    }
+    let daysInMonth = new Date(year, month, 0).getDate()
+    for (let i = 1; i <= daysInMonth; i++) {
+        let newDate = document.createElement("div")
+        newDate.textContent = `${i}-${month}-${year}`
+        newDate.setAttribute("id", `${i}-${month}-${year}`)
+        calender.appendChild(newDate)
+    }
+})
+
+const nextMonth = document.querySelector("#nextMonth")
+nextMonth.addEventListener("click",()=>{
+    calender.replaceChildren()
+    if(month<12)
+        month++
+    else if(month==12){
+        month = 1
+        year++
+    }
+    let daysInMonth = new Date(year, month, 0).getDate()
+    for (let i = 1; i <= daysInMonth; i++) {
+        let newDate = document.createElement("div")
+        newDate.textContent = `${i}-${month}-${year}`
+        newDate.setAttribute("id", `${i}-${month}-${year}`)
+        calender.appendChild(newDate)
     }
 })

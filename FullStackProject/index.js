e5659908551cd764f7796647f741c1ba0b0df1b1
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2026-06-30 17:55:51 +0530

    created first backend project

--- FullStackProject/index.js
+++ FullStackProject/index.js
@@ -1,36 +1,30 @@
-import express from 'express';
-import dotenv from "dotenv"
-dotenv.config()
-import db from "./utils/db.js"
-
+import express from "express"
+import dotenv from "dotenv/config"
 import cors from "cors"
-
-import dns from "dns"
-dns.setServers([
-  '1.1.1.1',
-  '8.8.8.8'
-])
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors({
-  origin: process.env.BASE_URL,
-  credentials: true,
-  methods: ['GET','POST', 'DELETE','OPTIONS'], // not case-sensitive
-  allowedHeaders: ['Content-Type', 'Authorization'] // case-sensitive
+  origin: "http://localhost:3000",
+   credentials: true,
+  methods: ['GET','POST', 'DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 app.use(express.json())
+
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {
-  res.send('Cohort')
+  res.send('Cohort!')
 })
 
-// connect to db
-db()
+app.get('/ishan',(req, res)=>{
+    res.send("ishan chandrakar!")
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+console.log(process.env.PORT);
